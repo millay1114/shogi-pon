d4a1bc4132fd73ac9929422187113c5bbfa7fe8b
millay1114 <millay1114@gmail.com>
2016-12-01 22:56:26 +0900

    first commit

---
+++ src/main/java/jp/co/pon/shogi/ShogiPonApplication.java
@@ -0,0 +1,12 @@
+package jp.co.pon.shogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShogiPonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShogiPonApplication.class, args);
+	}
+}
